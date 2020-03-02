21a789147846a83aa0fef9be3c7263b8776376cb
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-03-02 23:58:46 +0530

    #34 Parsing Request Bodies

--- Section3 - Understanding the Basics/app.js
+++ Section3 - Understanding the Basics/app.js
@@ -1,19 +1,29 @@
 const http = require('http');
 const fs = require('fs');
-//git add . | git commit -m "#32 Routing Requests" | git push -u origin master
+//git add . | git commit -m "#33 Redirecting Requests" | git push -u origin master
 const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if(url === '/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
     
     if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'DUMMY');
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
