dcd8ff659d97ef68419ba1968bc1af82a545eabd
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-03-03 10:50:38 +0530

    #35 Understanding Event Driven Code Execution

--- Section3 - Understanding the Basics/app.js
+++ Section3 - Understanding the Basics/app.js
@@ -1,7 +1,7 @@
-const http = require('http');
-const fs = require('fs');
-//git add . | git commit -m "#33 Redirecting Requests" | git push -u origin master
-const server = http.createServer((req, res) => {
+import { createServer } from 'http';
+import { writeFileSync } from 'fs';
+
+const server = createServer((req, res) => {
     const url = req.url;
     const method = req.method;
     if(url === '/'){
@@ -18,15 +18,14 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            writeFileSync('message.txt', message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
         });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
