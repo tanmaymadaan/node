54be4278547cca98a8afb764e8e3652377f428cf
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-03-04 02:42:41 +0530

    Assignment 1 Task 3 & 4

--- Assignment 1/routes.js
+++ Assignment 1/routes.js
@@ -1,14 +1,18 @@
 const requestHandler = (req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Greetings</title></head>');
-        res.write('<body><h1>Hi there, Welcome to the website!</h1></body>');
+        res.write('<body><h1>Hi there, Welcome to the website!</h1>');
+        res.write('<form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>')
         res.write('</html>');
         return res.end();
     }
 
     if(url === '/users'){
+        res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Greetings</title></head>');
         res.write('<body><ul><li>Tanmay</li><li>Yolo</li><li>haha</li></ul></body>');
@@ -16,12 +20,22 @@ const requestHandler = (req, res) => {
         return res.end();
     }
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server</h1></body>');
-    res.write('</html>');
-    res.end();
+    if(url === '/create-user' && method === 'POST'){
+        res.setHeader('Content-Type', 'text/html');
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const username = parsedBody.split('=')[1];
+            console.log(username);
+        });
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
 }
 
 exports.handler = requestHandler;
