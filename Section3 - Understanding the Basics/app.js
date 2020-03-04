bd4284d4d2fdd5ce054bdd9a566032f6287feb61
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-03-04 12:13:26 +0530

    Minor changes

--- Section3 - Understanding the Basics/app.js
+++ Section3 - Understanding the Basics/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
 const routes = require('./routes');
 console.log(routes.someText);
-const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 server.listen(3000);
