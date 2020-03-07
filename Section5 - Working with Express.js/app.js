81c2944d616ae3a2f9526662c0ae7c30843a4276
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-03-07 14:27:10 +0530

    #74 Serving Files Statically

--- Section5 - Working with Express.js/app.js
+++ Section5 - Working with Express.js/app.js
@@ -9,6 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -16,5 +17,4 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
 app.listen(3000);
